e70e89002e849562e35b73f5a0ade86951325363
Craig Douglas <59148559+Craig-Douglas@users.noreply.github.com>
2021-05-01 12:14:15 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -9,21 +9,21 @@ export default function App() {
   const [tasks, setTasks] = useState([
     {
       id: 2,
-      text: "blah blah blah, example job",
-      day: " Tues 25th Jillember",
+      text: "Review Finances",
+      day: " Today 10AM",
       reminder: true
     },
   { 
       id: 1,
-      text: "blah another example blah blah...",
-      day: "Friday 4PM.",
+      text: "Book Holiday",
+      day: "Today 1PM.",
       reminder: true
     },
 
     {
       id: 3,
-      text: "If I'm all thats left..... go drink a beer!!",
-      day: "Now, right now.",
+      text: "Property Maintenance",
+      day: "Today 3PM.",
       reminder: true
     }
   ]);
@@ -51,17 +51,19 @@ export default function App() {
 
   return (
     <div className="container">
+
       <Header
         onAdd={() => setShowAddTask
         (!showAddTask)}
         showAdd={showAddTask}
       />
+     
       {showAddTask && <AddTask onAdd={addTask} />}
 
       {tasks.length ? (
         <Tasks tasks={tasks} onDelete={deleteTasks} onToggle={toggleReminder} />
       ) : (
-        "ADD SOME JOBS - GET THEM DONE - GET A BEER!"
+        "Increase your productivity - complete 3 tasks per day!"
       )}
     </div>
   );
