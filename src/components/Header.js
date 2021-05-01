1317a1264633ca398db3cac95138bfee64311a7f
Craig Douglas <59148559+Craig-Douglas@users.noreply.github.com>
2021-05-01 12:27:00 +0100

    Add files via upload

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ const Header = ({ title, onAdd, showAdd }) => {
   return (
         <header className='header'>
             <h1>{title}</h1>
-            <Button color={showAdd ? 'white' : '#e33034'} text={showAdd ? 'Close' : 'Add'} npnonClick={onAdd}  />
+            <Button color={showAdd ? 'white' : '#e33034'} text={showAdd ? 'Close' : 'Add'} onClick={onAdd}  />
         </header>
     )
 }
